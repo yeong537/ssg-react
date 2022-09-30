a8602793060617e4550c4a11a402727eadc89b00
yamoo9 <yamoo9@naver.com>
2022-09-30 21:15:47 +0900

    learn. JSX with Babel
    - Babel 독립형 빌드 (런타임 컴파일러)
    - JSX 비표준 (브라우저 해석 못함)
    - JSX 개발자 경험(DX) 향상

---
+++ public/assets/learn-react-with-babel.js
@@ -0,0 +1,50 @@
+const container = document.getElementById('root');
+
+// React Element vs. React Component
+// Object vs. Class / Function
+
+// React Component → React Element 생성
+
+const Logo = () => (
+  <img
+    className="Logo"
+    src="assets/sinsegae.png"
+    alt="신세계 I&amp;C"
+    height="60"
+  />
+);
+
+const Headline = () => (
+  <h2 className="Headline" lang="en">
+    Premier IT Partner &amp; Value Creator
+  </h2>
+);
+
+const Description = () => (
+  <p className="Description">
+    최고 수준의 IT서비스로 고객 비즈니스 성공의 핵심 파트너로
+    성장해나가겠습니다.
+  </p>
+);
+
+// React.createElement('div')
+// React.createElement(Logo)
+// React.createElement(React.Framgment)
+
+const App = () => (
+  <div className="App">
+    <Logo />
+    <Headline />
+    <Description />
+  </div>
+);
+
+// JSX = XML like syntax for ECMAScript(JavaScript)
+
+// React.createElement(React.StrictMode)
+
+ReactDOM.createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
