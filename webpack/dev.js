64ff6a801a0ef33e7daada4b4adb7c74582f3e64
yamoo9 <yamoo9@naver.com>
2022-10-05 21:14:52 +0900

    learn. Webpack 개발 서버 구동 커멘드 추가

--- webpack/dev.js
+++ webpack/dev.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const commonConfig = require('./common');
+const serverConfig = require('./server');
 
 const devConfig = merge(commonConfig, {
   mode: 'development',
   devtool: 'eval-cheap-source-map',
+  devServer: serverConfig,
   output: {
     path: path.resolve('public'),
     filename: '[name].js',
--- webpack/server.js
+++ webpack/server.js
@@ -1,5 +1,20 @@
+const path = require('path');
+
 const serverConfig = {
+  // static: ['public'],
+  static: {
+    directory: path.resolve('public'),
+  },
+  client: {
+    overlay: true,
+  },
+  hot: true,
+  compress: true,
+  liveReload: true,
+  historyApiFallback: true,
+  host: 'localhost',
   port: 3000,
+  open: false,
 };
 
 module.exports = serverConfig;
